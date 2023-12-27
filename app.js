3cd5330decb08b98b2594dbb23f83890c4e64505
Deepaksharma-39 <d33paksharma.39@gmail.com>
2023-12-27 14:44:08 +0530

    authentication done

---
+++ app.js
@@ -0,0 +1,29 @@
+import express from "express";
+import connection from "./config.js";
+import userModel from "./models/User.js";
+import Jwt from "jsonwebtoken";
+import authRouter from "./routes/auth.js";
+import userRouter from "./routes/user.js";
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Connect to MongoDB
+connection();
+
+// Parse JSON request body
+app.use(express.json());
+
+app.get("/",(req,res)=>{
+  res.send("Welcome to homepage")
+})
+
+// Define authentication routes
+app.use('/auth', authRouter);
+
+// Define user routes
+app.use('/user', userRouter);
+
+app.listen(PORT, () => {
+  console.log(`Server started on port ${PORT}`);
+});
